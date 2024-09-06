c1dca9479f03fd4cf00e8e27ad3cb8dcffddf9df
Daize Dong <dzdong2019@gmail.com>
2024-09-06 21:19:58 +0800

    Rewind Fix

--- src/llmtuner/compression/prune/layer_drop.py
+++ src/llmtuner/compression/prune/layer_drop.py
@@ -103,7 +103,7 @@ def get_layer_similarities(model: MixtralForCausalLM, dataloader: DataLoader, ac
 
                 # 🔍 Calculate similarity (output+input due to residual connection)
                 cos_sim = F.cosine_similarity(input_hidden_states, output_hidden_states, dim=-1)  # (total_token_num)
-                cos_sim = cos_sim.mean().abs() # 🔍 for non-norm
+                cos_sim = cos_sim.mean()
                 cos_sim = accelerator.reduce(cos_sim, reduction="mean")  # 🔍 All reduce across devices
                 accelerator.print(f'layer {i} similarity: {cos_sim.item()}')
 
